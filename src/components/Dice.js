8953372cf5505b33acc4c1569ac2d9d1768d7cb1
Kristyna Marvalova <k.marvalova@outlook.com>
2022-06-14 18:09:28 +0200

    css finished

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,10 +1,11 @@
 import React from "react";
+import '../css/dice.css';
 
 export default function Dice(props) {
 
     return (
-        <>
+        <div id="dice-canvas">
             {props.dice.map((dice)=> <div id={dice.id} className={`dice ${dice.stopped ? "stopped" : ""}`} key={dice.id} onClick={props.hold}>{dice.number}</div>)}
-        </>
+        </div>
     )
 }
--- src/components/Win.js
+++ src/components/Win.js
@@ -1,10 +1,27 @@
 import React from "react";
+import '../css/win.css'
+import ConfettiGenerator from "confetti-js";
 
 export default function Win (props) {
+
+    React.useEffect(() => {
+        const confettiSettings = { target: 'win' };
+        const confetti = new ConfettiGenerator(confettiSettings);
+        confetti.render();
+       
+        return () => confetti.clear();
+      }, []) 
+
     return (
         <>
-            <div id="win" class={`win ${props.win ? "" : "none"}`}>You win!</div>
-            {props.win ? <button onClick={props.newGame}>New Game</button> : ""}
+            <div className={`win-container ${props.win ? "" : "none"}`}>
+                <div className={`win-background ${props.win ? "" : "none"}`}></div>
+                <canvas id="win" className={`win ${props.win ? "" : "none"}`}></canvas>
+                <div className={`you-win ${props.win ? "" : "none"}`}>You win!</div>
+                <button className={`win-button ${props.win ? "" : "none"}`} onClick={props.newGame}>New Game</button>
+            </div>
         </>
+        
+        
     )
 }
