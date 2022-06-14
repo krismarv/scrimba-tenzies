bf06d9ca428238dd35e4dfbea2d626a38b6fd19f
Kristyna Marvalova <k.marvalova@outlook.com>
2022-06-14 08:23:20 +0200

    basic logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import Basics from './components/Basics';
+import Dice from './components/Dice'
 
 function App() {
+  // STATES
+  const [dice, setDice] = React.useState(diceInitial())
+
+  function diceInitial(){
+    let arr = []
+    for (let i=0; i<10; i++) {
+      let obj = {};
+      obj.id = i
+      obj.number = Math.floor(Math.random()*6+1);
+      obj.stopped = false
+      arr.push(obj)
+    }
+    return arr
+  }
+
+  function hold(event) {
+    setDice((prevDice)=>{
+      return prevDice.map((item) => {
+        if (item.id===parseInt(event.target.id)) {
+          return {...item, stopped:!item.stopped}
+        } else 
+        {
+          return {...item}
+        }
+      })
+    })
+  }
+
+  function roll(){
+    setDice((dice)=>{
+      return dice.map((item)=>{
+        if (!item.stopped) {
+          item.number = Math.floor(Math.random()*6+1);
+        }
+        return item
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='canvas'>
+      <Basics />
+      <Dice
+        dice={dice}
+        hold={hold} />
+      <button onClick={roll}>Roll the dice</button>
     </div>
   );
 }
